25344594cb4dd767a3cf014ab9aaef2f44c434e2
svavelsyra <68859611+svavelsyra@users.noreply.github.com>
2020-11-21 20:01:28 +0100

    Adding Tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,3 +1,4 @@
+import pickle
 import pytest
 
 @pytest.fixture
@@ -11,3 +12,10 @@ def vault_data_unlocked():
     return {'salt': b'\xa0\x9b\xb4\xbe\xd1\xb7\xe0t\x11\x9a\xd2\xe2\x10\x04\x03\x7f',
             'iterations': 1000000,
             'vault': ['test']}
+
+@pytest.fixture
+def locked_file(tmpdir, vault_data_locked):
+    file = tmpdir.join('testfile.txt')
+    with open(file, 'bw') as fh:
+        pickle.dump(vault_data_locked, fh)
+    return file
--- tests/unit_vault_test.py
+++ tests/unit_vault_test.py
@@ -1,3 +1,5 @@
+import pickle
+
 from tests.fixtures import *
 import acid_vault.vault as vault
 
@@ -14,5 +16,61 @@ def test_already_unlocked(vault_data_unlocked):
     v.unlock('testpass')
     assert 'test' in v.data['vault']
     
-def test_lock():
-    pass
+def test_lock(vault_data_unlocked):
+    v = vault.Vault()
+    v.data = vault_data_unlocked
+    v.lock('testpass')
+    try:
+        assert 'test' not in v.data['vault']
+    except TypeError:
+        # v.data is a binary string so should trhow error
+        pass
+    assert b'test' not in v.data['vault']
+    
+def test_load_data(vault_data_locked):
+    data = pickle.dumps(vault_data_locked)
+    v = vault.Vault()
+    v.load_data(data)
+    v.unlock('testpass')
+    assert 'test' in v.data['vault']
+
+def test_save_data(vault_data_locked):
+    v = vault.Vault()
+    v.data = vault_data_locked
+    data = v.save_data()
+    v2 = vault.Vault()
+    v2.load_data(data)
+    v2.unlock('testpass')
+    assert 'test' in v2.data['vault']
+
+def test_load_file(locked_file):
+    v = vault.Vault()
+    with open(locked_file, 'rb') as fh:
+        v.load_file(fh)
+    v.unlock('testpass')
+    assert 'test' in v.data['vault']
+
+def test_save_file(vault_data_locked, tmpdir):
+    v = vault.Vault()
+    v.data = vault_data_locked
+    f = tmpdir.join('testfile.txt')
+    with open(f, 'wb') as fh:
+        v.save_file(fh)
+    v2 = vault.Vault()
+    with open(f, 'rb') as fh:
+        v2.load_file(fh)
+    v2.unlock('testpass')
+    assert 'test' in v2.data['vault']
+
+def test_load_clear(tmpdir, vault_data_unlocked):
+    v = vault.Vault()
+    v._locked = False
+    v.data = vault_data_unlocked
+    v.data['vault'] = []
+    f = tmpdir.join('testfile.txt')
+    with open(f, 'w') as fh:
+        print('("test", )', file=fh)
+    with open(f, 'r') as fh:
+        v.load_clear(fh)
+    assert ("test", ) in v.data['vault']
+    
